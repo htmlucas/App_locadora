3324eba59e79e42e950104e6aa1ef87bf06689c7
Lucas <lucasmartinsde@gmail.com>
2022-03-23 18:20:54 -0300

    23-03-2022

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -45,35 +45,131 @@
             
             </div>
         </div>
-    <modal-component id="modalMarca" titulo="Adicionar Marca">
-        <template v-slot:conteudo>
-            <div class="form-group">
-                <input-container-component 
-                    titulo="Nome da marca" 
-                    id="novoNome"
-                    id-help="novoNomeHelp"
-                    texto-ajuda="Informe o Nome da Marca"> 
-                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
-                </input-container-component>
-            </div>
-            <div class="form-group">
-                <input-container-component 
-                    titulo="Imagem" 
-                    id="novoImagem"
-                    id-help="novoImagemHelp"
-                    texto-ajuda="Selecione uma imagem no formato PNG"> 
-                    <input type="file" class="form-control-file" id="novoImagem" aria-describedby="nomeHelp" placeholder="Seleciona uma Imagem">
-                </input-container-component>
-            </div>
-        </template>
-        <template v-slot:rodape>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Salvar</button>
-        </template>
-    </modal-component>
+        <!-- Inicio do modal -->
+        <modal-component id="modalMarca" titulo="Adicionar Marca">
+
+            <!-- Alertas -->
+            <template v-slot:alertas>
+                <alert-component tipo="success" :detalhes ='transacaoDetalhes' titulo="Cadastro realizado com sucesso!" v-if="transacaoStatus == 'adicionado'"></alert-component>
+                <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar a marca" v-if="transacaoStatus == 'erro'"></alert-component>
+            </template>
+
+            <!-- Conteudo com os inputs -->
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component 
+                        titulo="Nome da marca" 
+                        id="novoNome"
+                        id-help="novoNomeHelp"
+                        texto-ajuda="Informe o Nome da Marca"> 
+                        <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca" v-model="nomeMarca">
+                    </input-container-component>
+                    {{nomeMarca}}
+                </div>
+                <div class="form-group">
+                    <input-container-component 
+                        titulo="Imagem" 
+                        id="novoImagem"
+                        id-help="novoImagemHelp"
+                        texto-ajuda="Selecione uma imagem no formato PNG"> 
+                        <input type="file" class="form-control-file" id="novoImagem" aria-describedby="nomeHelp" placeholder="Seleciona uma Imagem" @change="carregarImagem($event)">
+                    </input-container-component>
+                    {{arquivoImagem}}
+                </div>
+            </template>
+
+            <!-- Rodapé com os botoes  -->
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+            </template>
+
+        </modal-component>
+
+        <button type="button" @click="carregarLista()">Teste</button>
     </div>
 </template>
 
 <script>
+    export default{
+    computed:{
+        token(){
+
+            let token = document.cookie.split(';').find(indice => {
+                return indice.includes('token=')
+            })
+            token = token.split('=')[1]
+            token = 'Bearer '+ token
+            return token
+        }
+    },
+    data(){
+        return{
+            urlBase: 'http://localhost:8000/api/v1/marca',
+            nomeMarca:'',
+            arquivoImagem:[],
+            transacaoStatus:'',
+            transacaoDetalhes: {},
+            marcas: []
+        }
+    },
+    methods: {
+        carregarLista(){
+
+            let config = {
+                headers:{
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+            axios.get(this.urlBase,config)
+                .then(response =>{
+                    this.marcas =response.data
+                    console.log(this.marcas)
+                })
+                .catch(errors =>{
+                    console.log(errors)
+                })
+        },
+        
+        carregarImagem(e){
+            this.arquivoImagem = e.target.files
+        },
+        salvar(){
+
+            let formData = new FormData();
+            formData.append('nome',this.nomeMarca)
+            formData.append('imagem',this.arquivoImagem[0])
+
+            let config = {
+                headers:{
+                    'Content-Type':'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+            axios.post(this.urlBase,formData,config)
+                .then(response => {
+                    this.transacaoStatus = 'adicionado'
+                    this.transacaoDetalhes = {
+                        mensagem:'ID do registro:'+response.data.id
+                    }
+                    console.log(response)
+                })
+                .catch(errors => {
+                    this.transacaoStatus ='erro'
+                    this.transacaoDetalhes = {
+                        mensagem: errors.response.data.message,
+                        dados:errors.response.data.errors
+                    }
+                })
+        }
+    },
+    mounted(){
+        this.carregarLista()
+    }  
+    }
  
 </script>
